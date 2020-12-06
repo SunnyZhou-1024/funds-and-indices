7011f4fad9791736d936492f7fa036580eecb56a
SunnyZhou-1024 <sunnyzhou.1024@outlook.com>
2020-12-06 20:51:42 +0800

    Add reentry mechanism

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
 import csv
-import sys
 import os
+import sys
+import time
+
 from funds import *
 
 def ensure_enviroments():
@@ -8,18 +11,46 @@ def ensure_enviroments():
         os.mkdir('data')
 
 def parse_args():
-    pass
+    parser = argparse.ArgumentParser(description='Parse arguments of this program.')
+    parser.add_argument('--reentry', default=True, help='Start from the stop point of most recent time.')
+    return parser.parse_args()
 
 
 def main():
     args = parse_args()
+    checkpoint = []
+    all_funds = None
+    if args.reentry and os.path.exists(os.path.join('data', 'checkpoint.txt')):
+        with open(os.path.join('data', 'checkpoint.txt'), 'r') as f:
+            fetched = f.read()
+            if fetched != '':
+            # TO-DO: Should replace with a most rubost reentry method
+                checkpoint.extend(json.loads(fetched))
+            #checkpoint = int(fetched)
+        
+        # If enable reentry, we should read the fund list info from file rather than make request 
+        # But now, just make request.
+        #with open(os.path.join('data', '.txt'), 'r') as f:
+
+    else:
+        pass
     all_funds = get_all_funds_code_and_name()
-    funds_info = open(os.path.join('data', 'fundsdetails.json'), 'w')
+
+
+    timestamp = time.time()
+    timestamp = str(int(timestamp))
+    funds_info = open(os.path.join('data', 'fundsdetails-%s.json' % timestamp), 'w')
+    checkpoint_file = open(os.path.join('data', 'checkpoint.txt'), 'w')
     #funds_info_csv = csv.DictWriter(funds_info, ['基金代码'， '基金名字'， '成立日期',
     #                                        '基金经理', '基金规模'， '跟踪指数', '基金类型', ''])
     keys = ['基金代码', '基金名称拼音首字母', '基金名称', '基金类型', '基金名称全拼']
+
+    #all_funds = all_funds[checkpoint: ]
     for item in all_funds:
+        if item[0] in checkpoint:
+            continue
         try:
+            checkpoint.append(item[0])
             basic_info = fetch_fund_basic_info(item[0])
             fees = fetch_fees(item[0])
             history = fetch_net_worth_history(item[0], item[2], basic_info['成立日期'], None)
@@ -34,9 +65,15 @@ def main():
             funds_info.write('\n')
             funds_info.flush()
 
+            checkpoint_file.write(json.dumps(checkpoint))
+            checkpoint_file.flush()
+
         except Exception as e:
             print(str(e))
             pass
+
+    funds_info.close()
+    checkpoint_file.close()
 
 
 
