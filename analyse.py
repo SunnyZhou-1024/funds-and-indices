8c3f62b9f9b8dfd2cc49e204a9eba689a77aa7f0
SunnyZhou-1024 <sunnyzhou.1024@outlook.com>
2020-12-25 01:02:09 +0800

    Analysis

--- analyse.py
+++ analyse.py
@@ -11,7 +11,29 @@ class InfoType(Enum):
     BASIC_INFO = 2
     INDEX = 3
 
+class History(object):
+    def __init__(self, file, managers):
+        self._file = file
+        self._managers = managers
+
+    def _calc_max_retracement():
+
+    def _load_history(file):
+        hist = []
+        with open(file, 'r') as f:
+            line = f.readline()
+            obj = json.loads(line)
+            part = obj['LSJZList']
+            for p in part:
+                hist.append((p['FSRQ'], p['']))
+
+
+
+    def _key(pair):
+        return pair[0]
+
 def load(file=details_of_fund_file, info_type=InfoType.ALL)
+
     pass
 
 if __name__ == '__main__':
